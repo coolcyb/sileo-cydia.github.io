e46cd69b7808c7c83e978abb1dbb12b4bb4a497f
他是个疯子 <409774743@qq.com>
2019-03-15 18:10:11 +0800

    .

--- ms/msxq/cydia.sh
+++ ms/msxq/cydia.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/852359/Flex 3 Beta「强大的补丁平台」/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852359/HomeGesture「X手势」/g"  cydia副本 sileo副本.json
 #描述
-perl -p -i -e "s/852360/去软件广告,或者去软件启动广告,禁用某些系统功能,等等/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852360/HomeGesture修复微信语音bug,相机退出BUG，带设置开关，修复iOS12控制中心奔溃,X状态栏/g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/852362/IOS:11-12/g"  cydia副本 sileo副本.json
 
 
-mv cydia副本  Flex3beta
-mv sileo副本.json Flex3beta.json
+mv cydia副本  HomeGesture
+mv sileo副本.json HomeGesture.json
 
 
 cp cydia样本 cydia副本
