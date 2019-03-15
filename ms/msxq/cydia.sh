60463c16a4d2e5f04498a47fa8683c3856a07ba4
他是个疯子 <409774743@qq.com>
2019-03-15 19:07:35 +0800

    .

--- ms/msxq/cydia.sh
+++ ms/msxq/cydia.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/852359/HomeGesture「X手势」/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852359/Filza File Manager/g"  cydia副本 sileo副本.json
 #描述
-perl -p -i -e "s/852360/HomeGesture修复微信语音bug,相机退出BUG，带设置开关，修复iOS12控制中心奔溃,X状态栏/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852360/ios的文件管理器./g"  cydia副本 sileo副本.json
 #支持系统
-perl -p -i -e "s/852362/IOS:11-12/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852362/ios:11-12/g"  cydia副本 sileo副本.json
 
 
-mv cydia副本  HomeGesture
-mv sileo副本.json HomeGesture.json
+mv cydia副本  filza
+mv sileo副本.json filza.json
 
 
 cp cydia样本 cydia副本
