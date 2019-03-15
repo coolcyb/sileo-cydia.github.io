0e5b504d8096e2215fa1514ad3f7d34c86d27255
他是个疯子 <409774743@qq.com>
2019-03-16 00:14:01 +0800

    .

--- ms/msxq/cydia.sh
+++ ms/msxq/cydia.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/852359/WeChat「突破朋友圈视频30秒」/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852359/NtSpeed「虚浮网速」/g"  cydia副本 sileo副本.json
 #描述
-perl -p -i -e "s/852360/突破微信可拍摄与上传30秒视频・安装后无设置/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852360/让你在屏幕上看到当前的网络速度/g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/852362/ios:11-12/g"  cydia副本 sileo副本.json
 
 
-mv cydia副本 wechatfix30s
-mv sileo副本.json wechatfix30s.json
+mv cydia副本 ntspeed
+mv sileo副本.json ntspeed.json
 
 
 cp cydia样本 cydia副本
