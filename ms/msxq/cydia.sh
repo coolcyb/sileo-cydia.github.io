ca1aef3b217b06b8cb8c106221c607277b558502
他是个疯子 <409774743@qq.com>
2019-03-16 00:45:42 +0800

    .

--- ms/msxq/cydia.sh
+++ ms/msxq/cydia.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/852359/NtSpeed「虚浮网速」/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852359/Notifica「美化通知」/g"  cydia副本 sileo副本.json
 #描述
-perl -p -i -e "s/852360/让你在屏幕上看到当前的网络速度/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852360/让自定义通知的颜色.可以根据app图标颜色来定义通知的颜色/g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/852362/ios:11-12/g"  cydia副本 sileo副本.json
 
 
-mv cydia副本 ntspeed
-mv sileo副本.json ntspeed.json
+mv cydia副本 notifica
+mv sileo副本.json notifica.json
 
 
 cp cydia样本 cydia副本
