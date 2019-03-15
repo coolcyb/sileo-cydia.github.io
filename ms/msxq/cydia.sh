5d95977b65b35471298e983e377ce586a09a41f0
他是个疯子 <409774743@qq.com>
2019-03-15 18:00:00 +0800

    .

--- ms/msxq/cydia.sh
+++ ms/msxq/cydia.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/852359/ChargeAnimations「充电动画」/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852359/Flex 3 Beta「强大的补丁平台」/g"  cydia副本 sileo副本.json
 #描述
-perl -p -i -e "s/852360/插入充电器时以动画显示p佬破解版/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852360/去软件广告,或者去软件启动广告,禁用某些系统功能,等等/g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/852362/IOS:11-12/g"  cydia副本 sileo副本.json
-#版本
-perl -p -i -e "s/852361/1.2.1/g"  cydia副本 sileo副本.json
-#大小
-perl -p -i -e "s/000000/000000/g"  cydia副本 sileo副本.json
 
-mv cydia副本  ChargeAnimations
-mv sileo副本.json ChargeAnimations.json
+
+mv cydia副本  Flex3beta
+mv sileo副本.json Flex3beta.json
 
 
 cp cydia样本 cydia副本
