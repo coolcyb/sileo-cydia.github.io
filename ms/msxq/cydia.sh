a43a5551d04a8506515a0a1a455129b33b801000
他是个疯子 <409774743@qq.com>
2019-03-16 10:19:12 +0800

    .

--- ms/msxq/cydia.sh
+++ ms/msxq/cydia.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/852359/Notifica「美化通知」/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852359/Settings+「界面美化」/g"  cydia副本 sileo副本.json
 #描述
-perl -p -i -e "s/852360/让自定义通知的颜色.可以根据app图标颜色来定义通知的颜色/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852360/隐藏系统更新1小红点、去设置分割线.去设置顶部搜索/g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/852362/ios:11-12/g"  cydia副本 sileo副本.json
 
 
-mv cydia副本 notifica
-mv sileo副本.json notifica.json
+mv cydia副本 settings+
+mv sileo副本.json settings+.json
 
 
 cp cydia样本 cydia副本
