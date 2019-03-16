900f2789fb90dc6375c74dd7c1694f04d074922b
他是个疯子 <409774743@qq.com>
2019-03-17 00:07:43 +0800

    .

--- ms/msxq/cydia.sh
+++ ms/msxq/cydia.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/852359/Settings+「界面美化」/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852359/NewGridSwitcher「多任务后台」/g"  cydia副本 sileo副本.json
 #描述
-perl -p -i -e "s/852360/隐藏系统更新1小红点、去设置分割线.去设置顶部搜索/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852360/可以让 iPhone 拥有 iPad iOS 11/12 一样的多任务卡片式后/g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/852362/ios:11-12/g"  cydia副本 sileo副本.json
 
 
-mv cydia副本 settings+
-mv sileo副本.json settings+.json
+mv cydia副本 newgridswitcher
+mv sileo副本.json newgridswitcher.json
 
 
 cp cydia样本 cydia副本
