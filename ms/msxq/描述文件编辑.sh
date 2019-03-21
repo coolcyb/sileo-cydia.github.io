bf3ece68b5cedaa3ad8b5231ff440266502efe9b
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-03-21 21:17:13 +0800

    .

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/852359/SwipeToDeleteContact「删除联系人」/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852359/Shy Page Dots「隐藏小白点」/g"  cydia副本 sileo副本.json
 #描述
-perl -p -i -e "s/852360/在通讯录右滑删除联系人！/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852360/静止时隐藏桌面的小白点！/g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/852362/ios:11-12/g"  cydia副本 sileo副本.json
 
 
-mv cydia副本 swipetodeletecontact
-mv sileo副本.json swipetodeletecontact.json
+mv cydia副本 shypagedots
+mv sileo副本.json shypagedots.json
 
 
 cp cydia样本 cydia副本
