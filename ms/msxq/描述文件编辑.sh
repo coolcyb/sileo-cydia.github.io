47ac82df169a95a4212f3753c76d8b344cc83ec6
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-03-22 11:07:58 +0800

    .

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/Auto HomeBar「隐藏X白条」/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/AudioRecorder XS「通话录音」/g"  cydia副本 sileo副本.json
 #描述
-perl -p -i -e "s/852360/将iPhoneX底部的小黑条/白条 在没有任何动作的时候自动隐藏！适合喜欢Bar且不想去掉,但是又觉得看视频的时候会遮挡的机友.无设置！/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852360/通话录音首次安装.打开设置>找到通话录音的设置>修复一下文档权限！/g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/852362/ios:11-12/g"  cydia副本 sileo副本.json
 
 
-mv cydia副本 autohomebar
-mv sileo副本.json autohomebar.json
+mv cydia副本 audiorecorderxs
+mv sileo副本.json audiorecorderxs.json
 
 
 cp cydia样本 cydia副本
