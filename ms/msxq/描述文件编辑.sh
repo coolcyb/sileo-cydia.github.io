ac8fbd72d47ba2dba97442eff6628c2d8c3d7940
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-04-30 21:52:09 +0800

    .

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/852359/WeChat Helper/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852359/AutoLockToggle「锁定开关」/g"  cydia副本 sileo副本.json
 #描述
-perl -p -i -e "s/852360/微信多功能定义助手/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852360/在控制中心添加,打开/关闭,自动锁定开关.需配合ccsupport/g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/852362/ios:11-12/g"  cydia副本 sileo副本.json
+#更新时间
+perl -p -i -e "s/2019-01-21/2019-04-30/g"  cydia副本 sileo副本.json
 
 
-mv cydia副本 wechathelper
-mv sileo副本.json wechathelper.json
+mv cydia副本 autolockcc
+mv sileo副本.json autolockcc.json
 
 
 cp cydia样本 cydia副本
