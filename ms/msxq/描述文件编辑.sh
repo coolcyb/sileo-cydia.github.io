90815e3cfac2958c7ccb0d1023ce1366e94382b1
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-04-30 14:44:55 +0800

    .

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/AudioRecorder XS「通话录音」/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/ReProvision「签名ipa」/g"  cydia副本 sileo副本.json
 #描述
-perl -p -i -e "s/852360/通话录音首次安装.打开设置>找到通话录音的设置>修复一下文档权限！/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852360/手机端签名ipa工具/g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/852362/ios:11-12/g"  cydia副本 sileo副本.json
 
 
-mv cydia副本 audiorecorderxs
-mv sileo副本.json audiorecorderxs.json
+mv cydia副本 reprovision
+mv sileo副本.json reprovision.json
 
 
 cp cydia样本 cydia副本
