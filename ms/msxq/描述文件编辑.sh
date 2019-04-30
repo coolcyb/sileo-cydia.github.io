763111044f2aff32d0420d8095cccc2e7907c284
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-04-30 16:57:13 +0800

    .

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/ReProvision「签名ipa」/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852359/WeChat Helper/g"  cydia副本 sileo副本.json
 #描述
-perl -p -i -e "s/852360/手机端签名ipa工具/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852360/微信多功能定义助手/g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/852362/ios:11-12/g"  cydia副本 sileo副本.json
 
 
-mv cydia副本 reprovision
-mv sileo副本.json reprovision.json
+mv cydia副本 wechathelper
+mv sileo副本.json wechathelper.json
 
 
 cp cydia样本 cydia副本
