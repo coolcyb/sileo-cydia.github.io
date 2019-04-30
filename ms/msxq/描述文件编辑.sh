74cdb2cf35211b2abc80653e900857e636333394
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-04-30 22:29:27 +0800

    .

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/852359/ccsupport「控制中心依赖文件」/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852359/EmojiPort「iOS 12」/g"  cydia副本 sileo副本.json
 #描述
-perl -p -i -e "s/852360/控制中心小部件的依赖程序/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852360/键盘下面添加最近使用的表情快捷栏/g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/852362/ios:11-12/g"  cydia副本 sileo副本.json
 #更新时间
 perl -p -i -e "s/2019-01-21/2019-04-30/g"  cydia副本 sileo副本.json
 
 
-mv cydia副本 ccsupport
-mv sileo副本.json ccsupport.json
+mv cydia副本 emojiportpe
+mv sileo副本.json emojiportpe.json
 
 
 cp cydia样本 cydia副本
