05b56909d9c9a5f94c3817ed1aa4c45a62b89f36
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-05-01 18:28:50 +0800

    .

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/852359/DoubleTapLock「双击锁屏」/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852359//g"  cydia副本 sileo副本.json
 #描述
-perl -p -i -e "s/852360/双击屏幕空白处快速锁屏.有问题联系qq群77148984.2019-05-01更新支持A12./g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852360/.有问题联系qq群77148984.2019-05-01更新支持A12./g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/852362/ios:11-12/g"  cydia副本 sileo副本.json
 #更新时间
@@ -15,8 +15,8 @@ perl -p -i -e "s/4.png/4.png/g"   sileo副本.json
 perl -p -i -e "s/ipX.gif/ipX.gif/g"  cydia副本
 
 
-mv cydia副本 doubletaplock
-mv sileo副本.json doubletaplock.json
+mv cydia副本 
+mv sileo副本.json .json
 
 
 cp cydia样本 cydia副本
