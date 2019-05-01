45601925492d1fcaaacca31dcc915d7f1f781124
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-05-01 22:34:56 +0800

    .

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -2,7 +2,7 @@
 标题
 perl -p -i -e "s/852359/UnSub「屏蔽越狱检测」/g"  cydia副本 sileo副本.json
 #描述
-perl -p -i -e "s/852360/屏蔽越狱检测,这个比较安全,目前还没有发现任何bug和app屏蔽失效.有问题联系qq群77148984.2019-05-01更新支持A12./g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852360/屏蔽越狱检测,这个比较安全,目前还没有发现任何bug和app屏蔽失效.2019-05-01更新支持A12./g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/852362/ios:11-12/g"  cydia副本 sileo副本.json
 #更新时间
