fb66b8166005bce98e94057ec96d56437b20ecd8
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-05-01 09:14:16 +0800

    .

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/852359/EmojiPort「iOS 12」/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852359/pencilchargingindicator/g"  cydia副本 sileo副本.json
 #描述
-perl -p -i -e "s/852360/键盘下面添加最近使用的表情快捷栏/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852360/充电动画特效,支持A12/g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/852362/ios:11-12/g"  cydia副本 sileo副本.json
 #更新时间
-perl -p -i -e "s/2019-01-21/2019-04-30/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/2019-01-21/2019-05-01/g"  cydia副本 sileo副本.json
 
 
-mv cydia副本 emojiportpe
-mv sileo副本.json emojiportpe.json
+mv cydia副本 pencilchargingindicator
+mv sileo副本.json pencilchargingindicator.json
 
 
 cp cydia样本 cydia副本
