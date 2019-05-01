bed991eb32d14a181b09f8459a055f5cab401746
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-05-01 23:25:48 +0800

    .

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/852359/UnSub「屏蔽越狱检测」/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852359/FloatingDockPlus「悬浮dock分屏」/g"  cydia副本 sileo副本.json
 #描述
-perl -p -i -e "s/852360/屏蔽越狱检测,这个比较安全,目前还没有发现任何bug和app屏蔽失效.2019-05-01更新支持A12./g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852360/像ipad一样的dock栏,还可以分屏./g"  cydia副本 sileo副本.json
 #支持系统
-perl -p -i -e "s/852362/ios:11-12/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852362/IOS:11-12/g"  cydia副本 sileo副本.json
 #更新时间
-perl -p -i -e "s/2019-01-21/2019-05-01/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/更新时间/2019-05-01更新支持A12/g"  sileo副本.json
+perl -p -i -e "s/暂时没有历史版本/2019-05-01更新支持A12/g"  cydia副本
 #截图
-perl -p -i -e "s/1.png/unsub1.png/g"   sileo副本.json
-perl -p -i -e "s/2.png/unsub2.png/g"   sileo副本.json
-perl -p -i -e "s/3.png/unsub1.png/g"   sileo副本.json
-perl -p -i -e "s/4.png/unsub2.png/g"   sileo副本.json
-perl -p -i -e "s/ipX.gif/unsub1.png/g"  cydia副本
+perl -p -i -e "s/1.png/floatingdockplus1.png/g"   sileo副本.json
+perl -p -i -e "s/2.png/floatingdockplus2.png/g"   sileo副本.json
+perl -p -i -e "s/3.png/floatingdockplus1.png/g"   sileo副本.json
+perl -p -i -e "s/4.png/floatingdockplus2.png/g"   sileo副本.json
+perl -p -i -e "s/ipX.gif/floatingdockplus1.png/g"  cydia副本
 
 
-mv cydia副本 unsub
-mv sileo副本.json unsub.json
+mv cydia副本 floatingdockplus
+mv sileo副本.json floatingdockplus.json
 
 
 cp cydia样本 cydia副本
