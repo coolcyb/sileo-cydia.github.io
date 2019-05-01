3f089a699f8b810f7a093362866cf030458d42f6
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-05-01 10:23:28 +0800

    .

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/852359/pencilchargingindicator/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852359/Maple「充电通知」/g"  cydia副本 sileo副本.json
 #描述
-perl -p -i -e "s/852360/充电动画特效,支持A12/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852360/为每台连接到iphone的其他设备显示在你的iPhone主屏上/g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/852362/ios:11-12/g"  cydia副本 sileo副本.json
 #更新时间
 perl -p -i -e "s/2019-01-21/2019-05-01/g"  cydia副本 sileo副本.json
+#截图
+perl -p -i -e "s/1.png/maplebeta1.png/g"   sileo副本.json
+perl -p -i -e "s/2.png/2.png/g"   sileo副本.json
+perl -p -i -e "s/3.png/3.png/g"   sileo副本.json
+perl -p -i -e "s/4.png/4.png/g"   sileo副本.json
+perl -p -i -e "s/ipX.gif/maplebeta1.png/g"  cydia副本
 
 
-mv cydia副本 pencilchargingindicator
-mv sileo副本.json pencilchargingindicator.json
+mv cydia副本 maplebeta
+mv sileo副本.json maplebeta.json
 
 
 cp cydia样本 cydia副本
