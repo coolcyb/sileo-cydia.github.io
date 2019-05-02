67532e949df29f1606369d3f3e2b99844b343692
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-05-02 23:17:23 +0800

    .

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-标题
-perl -p -i -e "s/852359/KillX「快速关闭所有后台」/g"  cydia副本 sileo副本.json
-#描述
-perl -p -i -e "s/852360/开启后台卡片切换界面向下拉清理所有后台程序./g"  cydia副本 sileo副本.json
+perl -p -i -e "s/插件名字/AppStore++「应用降级」/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/插件的描述文字/降级appstore的app软件/g"  cydia副本 sileo副本.json
 #支持系统
-perl -p -i -e "s/852362/IOS:11-12/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/IOS:11-12/IOS:11-12/g"  cydia副本 sileo副本.json
 #更新时间
-perl -p -i -e "s/2019-01-21/2019-05-02/g"  sileo副本.json
+perl -p -i -e "s/2019-01-21/2019-05-03/g"  sileo副本.json
 perl -p -i -e "s/暂时没有历史版本/2019-05-02更新支持A12/g"  cydia副本
 #截图
 perl -p -i -e "s/1.png/1.png/g"   sileo副本.json
@@ -14,8 +12,6 @@ perl -p -i -e "s/2.png/2.png/g"   sileo副本.json
 perl -p -i -e "s/3.png/3.png/g"   sileo副本.json
 perl -p -i -e "s/4.png/4.png/g"   sileo副本.json
 perl -p -i -e "s/ipX.gif/ipX.gif/g"  cydia副本
-
-
 mv cydia副本 appstoreplusplus
 mv sileo副本.json appstoreplusplus.json
 
