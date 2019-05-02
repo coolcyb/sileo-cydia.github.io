1ef622f74ee3fcd0e608d3e03a63875ef3e5b059
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-05-02 23:25:10 +0800

    .

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 perl -p -i -e "s/插件名字/AppStore++「应用降级」/g"  cydia副本 sileo副本.json
-perl -p -i -e "s/插件的描述文字/降级appstore的app软件/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/插件的描述文字/降级appstore的app软件.长按-安装或者更新,来下载历史版本/g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/IOS:11-12/IOS:11-12/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/1.0.0/0.7-4/g"  cydia副本 sileo副本.json
 #更新时间
 perl -p -i -e "s/2019-01-21/2019-05-03/g"  sileo副本.json
 perl -p -i -e "s/暂时没有历史版本/2019-05-02更新支持A12/g"  cydia副本
