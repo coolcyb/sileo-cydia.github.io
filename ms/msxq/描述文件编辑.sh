552e9bde81d1ba5eeb31e1676d16aa25ee408dfc
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-05-03 02:48:00 +0800

    .

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-perl -p -i -e "s/插件名字/AppStore++「应用降级」/g"  cydia副本 sileo副本.json
-perl -p -i -e "s/插件的描述文字/降级appstore的app软件.长按-安装或者更新,来下载历史版本/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/插件名字/Filza File Manager「破解版」/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/插件的描述文字/文件管理器破解版/g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/IOS:11-12/IOS:11-12/g"  cydia副本 sileo副本.json
-perl -p -i -e "s/1.0.0/0.7-4/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/1.0.0/3.6.5-4/g"  cydia副本 sileo副本.json
 #更新时间
 perl -p -i -e "s/2019-01-21/2019-05-03/g"  sileo副本.json
-perl -p -i -e "s/暂时没有历史版本/2019-05-02更新支持A12/g"  cydia副本
+perl -p -i -e "s/暂时没有历史版本/2019-05-03更新支持A12/g"  cydia副本
 #截图
 #perl -p -i -e "s/1.png/appstoreplusplus1.png/g"   sileo副本.json
 #perl -p -i -e "s/2.png/appstoreplusplus2.png/g"   sileo副本.json
 #perl -p -i -e "s/3.png/appstoreplusplus1.png/g"   sileo副本.json
 #perl -p -i -e "s/4.png/appstoreplusplus2.png/g"   sileo副本.json
 #perl -p -i -e "s/ipX.gif/appstoreplusplus1.png/g"  cydia副本
-mv cydia副本 appstoreplusplus
-mv sileo副本.json appstoreplusplus.json
+mv cydia副本 fengziboy.filza
+mv sileo副本.json fengziboy.filza.json
 
 
 cp cydia样本 cydia副本
