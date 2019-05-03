ee2666be31b11d7c5a2947337c281bc86a67c386
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-05-03 18:18:08 +0800

    .

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-perl -p -i -e "s/插件名字/CallBarXS「ios12迷你通话横幅汉化破解版」」/g"  cydia副本 sileo副本.json
-perl -p -i -e "s/插件的描述文字/当你在做其他事情的时候不然来电打断你.破解版!/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/插件名字/Noctis12「暗黑模式」/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/插件的描述文字/全局的暗黑模式调整,暂时没法汉化,英文破解版!/g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/IOS:11-12/IOS:11-12/g"  cydia副本 sileo副本.json
-perl -p -i -e "s/1.0.0/0.5.1/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/1.0.0/1.6.2/g"  cydia副本 sileo副本.json
 #更新时间
 perl -p -i -e "s/2019-01-21/2019-05-03/g"  sileo副本.json
 perl -p -i -e "s/暂时没有历史版本/2019-05-03更新支持A12/g"  cydia副本
 #截图
-perl -p -i -e "s/1.png/callbarx.png/g"   sileo副本.json
-perl -p -i -e "s/2.png/callbarx1.png/g"   sileo副本.json
-perl -p -i -e "s/3.png/callbarx.png/g"   sileo副本.json
-perl -p -i -e "s/4.png/callbarx1.png/g"   sileo副本.json
-#perl -p -i -e "s/ipX.gif/callbarxs1.png/g"  cydia副本
-mv cydia副本 callbarxs
-mv sileo副本.json callbarxs.json
+perl -p -i -e "s/1.png/noctis122.png/g"   sileo副本.json
+perl -p -i -e "s/2.png/noctis121.png/g"   sileo副本.json
+perl -p -i -e "s/3.png/noctis122.png/g"   sileo副本.json
+perl -p -i -e "s/4.png/noctis121.png/g"   sileo副本.json
+perl -p -i -e "s/ipX.gif/noctis121.png/g"  cydia副本
+mv cydia副本 noctis12
+mv sileo副本.json noctis12.json
 
 
 cp cydia样本 cydia副本
