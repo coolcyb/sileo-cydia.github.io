18e6c42f01788679f117628a08421cc74e4eb499
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-05-03 14:27:55 +0800

    .

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-perl -p -i -e "s/插件名字/Xen HTML「高级锁屏汉化版」/g"  cydia副本 sileo副本.json
-perl -p -i -e "s/插件的描述文字/统一的HTML呈现；锁定屏幕和主屏幕.自定义锁屏!/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/插件名字/CallBarXS「ios12迷你通话横幅汉化破解版」」/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/插件的描述文字/当你在做其他事情的时候不然来电打断你.破解版!/g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/IOS:11-12/IOS:11-12/g"  cydia副本 sileo副本.json
 perl -p -i -e "s/1.0.0/0.5.1/g"  cydia副本 sileo副本.json
@@ -8,13 +8,13 @@ perl -p -i -e "s/1.0.0/0.5.1/g"  cydia副本 sileo副本.json
 perl -p -i -e "s/2019-01-21/2019-05-03/g"  sileo副本.json
 perl -p -i -e "s/暂时没有历史版本/2019-05-03更新支持A12/g"  cydia副本
 #截图
-perl -p -i -e "s/1.png/xenhtml1.png/g"   sileo副本.json
-perl -p -i -e "s/2.png/xenhtml1.png/g"   sileo副本.json
-perl -p -i -e "s/3.png/xenhtml1.png/g"   sileo副本.json
-perl -p -i -e "s/4.png/xenhtml1.png/g"   sileo副本.json
-perl -p -i -e "s/ipX.gif/xenhtml1.png/g"  cydia副本
-mv cydia副本 xenhtml
-mv sileo副本.json xenhtml.json
+perl -p -i -e "s/1.png/callbarx.png/g"   sileo副本.json
+perl -p -i -e "s/2.png/callbarx1.png/g"   sileo副本.json
+perl -p -i -e "s/3.png/callbarx.png/g"   sileo副本.json
+perl -p -i -e "s/4.png/callbarx1.png/g"   sileo副本.json
+#perl -p -i -e "s/ipX.gif/callbarxs1.png/g"  cydia副本
+mv cydia副本 callbarxs
+mv sileo副本.json callbarxs.json
 
 
 cp cydia样本 cydia副本
