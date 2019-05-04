b905e9737dd9b5280ae171cd8e300c2831762e94
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-05-05 00:45:20 +0800

    .

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-perl -p -i -e "s/插件名字/Slices 2「应用多开」汉化版」/g"  cydia副本 sileo副本.json
-perl -p -i -e "s/插件的描述文字/允许您为单个应用程序具有多个设置和数据包。/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/插件名字/shuffle「设置分类」汉化版/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/插件的描述文字/对设置里面的标题分类./g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/IOS:11-12/IOS:11-12/g"  cydia副本 sileo副本.json
-perl -p -i -e "s/1.0.0/slices2/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/1.0.0/0.9.6b/g"  cydia副本 sileo副本.json
 #更新时间
 perl -p -i -e "s/2019-01-21/2019-05-04/g"  sileo副本.json
 perl -p -i -e "s/暂时没有历史版本/2019-05-04更新支持A12/g"  cydia副本
@@ -13,8 +13,8 @@ perl -p -i -e "s/暂时没有历史版本/2019-05-04更新支持A12/g"  cydia副
 #perl -p -i -e "s/3.png/slices23.png/g"   sileo副本.json
 #perl -p -i -e "s/4.png/slices23.png/g"   sileo副本.json
 #perl -p -i -e "s/ipX.gif/slices21.png/g"  cydia副本
-mv cydia副本 slices2
-mv sileo副本.json slices2.json
+mv cydia副本 shuffle
+mv sileo副本.json shuffle.json
 
 
 cp cydia样本 cydia副本
