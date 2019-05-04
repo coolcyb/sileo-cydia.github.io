f7b94c6f0e871095ebe3e702e608427a0c3dc5b9
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-05-04 11:02:45 +0800

    .

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -8,13 +8,13 @@ perl -p -i -e "s/1.0.0/1.0.0/g"  cydia副本 sileo副本.json
 perl -p -i -e "s/2019-01-21/2019-05-03/g"  sileo副本.json
 perl -p -i -e "s/暂时没有历史版本/2019-05-03更新支持A12/g"  cydia副本
 #截图
-perl -p -i -e "s/1.png/Gesto1.png/g"   sileo副本.json
-perl -p -i -e "s/2.png/Gesto2.png/g"   sileo副本.json
-perl -p -i -e "s/3.png/Gesto3.png/g"   sileo副本.json
-perl -p -i -e "s/4.png/Gesto4.png/g"   sileo副本.json
+perl -p -i -e "s/1.png/gesto1.png/g"   sileo副本.json
+perl -p -i -e "s/2.png/gesto2.png/g"   sileo副本.json
+perl -p -i -e "s/3.png/gesto3.png/g"   sileo副本.json
+perl -p -i -e "s/4.png/gesto1.png/g"   sileo副本.json
 perl -p -i -e "s/ipX.gif/Gesto1.png/g"  cydia副本
-mv cydia副本 Gesto
-mv sileo副本.json Gesto.json
+mv cydia副本 gesto
+mv sileo副本.json gesto.json
 
 
 cp cydia样本 cydia副本
