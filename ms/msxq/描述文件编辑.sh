8398fbf7d1044ea69c6ff99e7c54caa99aa32ba3
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-05-05 15:35:47 +0800

    .

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-perl -p -i -e "s/插件名字/CCModules「控制中心增强」/g"  cydia副本 sileo副本.json
-perl -p -i -e "s/插件的描述文字/控制中心快捷开关/重启/注销/安全模式/等等/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/插件名字/CopyLog「粘贴增强」/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/插件的描述文字/粘贴版增强.粘贴历史记录,键盘上划暂时对A12无效,可以长按复制来使用.破解收费./g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/IOS:11-12/IOS:11-12/g"  cydia副本 sileo副本.json
-perl -p -i -e "s/1.0.0/1.4-2/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/1.0.0/1.1.2g"  cydia副本 sileo副本.json
 #更新时间
 perl -p -i -e "s/2019-01-21/2019-05-04/g"  sileo副本.json
-perl -p -i -e "s/暂时没有历史版本/2019-05-04更新支持A12/g"  cydia副本
+perl -p -i -e "s/暂时没有历史版本/2019-05-05更新支持A12/g"  cydia副本
 #截图
 #perl -p -i -e "s/1.png/slices21.png/g"   sileo副本.json
 #perl -p -i -e "s/2.png/slices22.png/g"   sileo副本.json
 #perl -p -i -e "s/3.png/slices23.png/g"   sileo副本.json
 #perl -p -i -e "s/4.png/slices23.png/g"   sileo副本.json
 #perl -p -i -e "s/ipX.gif/slices21.png/g"  cydia副本
-mv cydia副本 safeccmodules
-mv sileo副本.json safeccmodules.json
+mv cydia副本 copylog
+mv sileo副本.json copylog.json
 
 
 cp cydia样本 cydia副本
